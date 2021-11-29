70bbcc97b10f2e7c3380751fc6573f02f745467f
lansyfonseka <twinkstriker@gmail.com>
2021-11-30 00:38:45 +0300

    feat: create product item for main page

--- src/components/product-item/product-item.ts
+++ src/components/product-item/product-item.ts
@@ -1,12 +1,68 @@
+import './scss/product-item.style.scss';
+
 class ProductItemComponent {
-  constructor() {
+  id : string;
+  tier: string;
+  type: string;
+  name: string;
+  price: number;
+  nation: string;
+  images: Array<string>;
+  tankType: string;
+  size: number;
+  linkToDiscription: string;
+  discount: number;
+  flag: string;
+  constructor({tier, type, name, price, nation, images, tank_type, id}) {
+    this.id = id;
+    this.tier = this.convertToRomane(tier); // convert example "4" -> "IV"
+    this.type = type; //tank, gold ir premium
+    this.tankType = tank_type; //ligth, medium, haevy
+    this.name = name; // shor name tank
+    this.price = price; //default price $
+    this.nation = nation; //country 
+    this.flag = `flag__${this.nation}`; // for icon flag
+    this.images = images; // link image
+    this.size = 1; // add to JSON
+    this.linkToDiscription = `/tank/${this.id}`; // add to JSON
+    this.discount = 0; // add to JSON;
+
     this.init = this.init.bind(this);
     this.render = this.render.bind(this);
   }
 
-  init() {}
-
-  render() {}
+  convertToRomane(number:number):string {
+    let map = ['','I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
+    return map[number];
 }
 
-export default new ProductItemComponent();
+  init() {}
+
+  render() {
+    return `
+      <article class="card ${this.size}" id="${(this.id)}">
+        <a href="${(this.linkToDiscription)}" class="card-info">
+          <img class="card-img" src="${this.images[0]}" alt="${(this.name)}" />
+          <div class="card-specifications">
+            <p class="discount">${this.discount}</p>
+            <h2 class="item-text">
+              <span class="flag ${this.flag}"></span>
+              <span class="type ${this.tankType}"></span>
+              <span class="level">${this.tier}</span>
+              <span class="item-name">${this.name}</span>
+            </h2>
+            <p class="price">${this.price}</p>
+          </div>
+        </a>
+          <button class="like-btn">
+            <svg class="like-btn__icon">
+              <use xlink:href="assets/images/sprite.svg#like"></use>
+            </svg>
+          </button>
+          <button class="purchase-btn">purchase</button>
+      </article>
+    `;
+  }
+}
+
+export default ProductItemComponent;
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,30 @@
 import './scss/main.scss';
 
 import app from './components/app/app';
+import ProductItemComponent from './components/product-item/product-item';
 
 const appInit = () => {
   app.init();
   document.removeEventListener('DOMContentLoaded', appInit);
+  let fragment = document.createElement('div');  
+  let tank_1 = new ProductItemComponent(mainData);
+  fragment.innerHTML = tank_1.render();
+  document.body.append(fragment);
 };
 
 window.addEventListener('DOMContentLoaded', appInit);
+
+
+const mainData = {
+  tier: 5,
+  type: "machinery",
+  name: "T-34",
+  price: 71,
+  nation: "ussr",
+  images: [
+  "http://api-console.worldoftanks.com/static/2.70/wotx/encyclopedia/tanks/big_ussr_r04_t_34.png"
+  ],
+  details: "The legend of the Soviet armored forces and the most widely-produced Soviet tank of World War II, with a total of 33,805 vehicles manufactured. Three variants of this model were produced at several Soviet factories from 1940 through 1944.",
+  tank_type: "mediumTank",
+  id: "61a2d7ace2cd2550164b8144"
+}
