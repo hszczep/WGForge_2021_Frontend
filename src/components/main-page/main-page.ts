f939dc4ee5fdd5ee5d3bc0da2374a41b93b56898
Ivan Kuliasov <dfyreybr@gmail.com>
2021-11-30 20:57:39 +0300

    feat:add adaptive

--- src/components/main-page/main-page.ts
+++ src/components/main-page/main-page.ts
@@ -1,6 +1,6 @@
 import './scss/main-page.styles.scss';
 
-class mainPageComponent {
+class MainPageComponent {
   constructor() {
     this.render = this.render.bind(this);
     this.init = this.init.bind(this);
@@ -21,7 +21,7 @@ class mainPageComponent {
     return `
       <div class="content-field">
         <div class="content-menu">
-          <a href="#"><img src="assets/images/WoT_logo.png" alt="WoT logo" /></a>
+          <a href="#" class="WoT_logo"><img src="assets/images/WoT_logo.png" alt="WoT logo" /></a>
           <div class="content-menu__buttons">
             <button class="active-menu__button">All</button>
             <button>Vehicles</button>
@@ -34,14 +34,15 @@ class mainPageComponent {
             <a href="#" class="card-info">
               <img class="card-img" src="assets/images/is6.png" alt="ИС-6" />
               <div class="card-specifications">
-                <p class="discount"></p>
+                <p class="discount">-10%</p>
                 <h2 class="item-text">
                   <span class="flag flag__ussr"></span>
-                  <span class="type type__at-spg"></span>
+                  <span class="tank-type tank-type__at-spg"></span>
                   <span class="level">VIII</span>
                   <span class="item-name">ИС-6</span>
                 </h2>
-                <p class="price">$ 47.99</p>
+                <p class="price old-price">$ 20.99</p>
+                <p class="price price-discount">$ 13.43</p>
               </div>
             </a>
             <button class="like-btn">
@@ -58,11 +59,12 @@ class mainPageComponent {
                 <p class="discount"></p>
                 <h2 class="item-text">
                   <span class="flag flag__uk"></span>
-                  <span class="type type__heavytank"></span>
+                  <span class="tank-type tank-type__heavytank"></span>
                   <span class="level">VII</span>
                   <span class="item-name">M56 dgdrgr grdg drgdrgdrgrdgrd</span>
                 </h2>
-                <p class="price">$ 4.99</p>
+                <p class="price">$ 1.00</p>
+                <p class="price price-discount"></p>
               </div>
             </a>
             <button class="like-btn">
@@ -79,11 +81,12 @@ class mainPageComponent {
                 <p class="discount"></p>
                 <h2 class="item-text">
                   <span class="flag flag__czech"></span>
-                  <span class="type type__lighttank"></span>
+                  <span class="tank-type tank-type__lighttank"></span>
                   <span class="level">V</span>
                   <span class="item-name">M56</span>
                 </h2>
-                <p class="price">$ 147.99</p>
+                <p class="price">$ 66.99</p>
+                <p class="price price-discount"></p>
               </div>
             </a>
             <button class="like-btn">
@@ -100,11 +103,12 @@ class mainPageComponent {
                 <p class="discount"></p>
                 <h2 class="item-text">
                   <span class="flag flag__china"></span>
-                  <span class="type type__multirole"></span>
+                  <span class="tank-type tank-type__multirole"></span>
                   <span class="level">III</span>
                   <span class="item-name">ИС-9</span>
                 </h2>
-                <p class="price">$ 100.03</p>
+                <p class="price">$ 100.99</p>
+                <p class="price price-discount"></p>
               </div>
             </a>
             <button class="like-btn">
@@ -121,14 +125,34 @@ class mainPageComponent {
                 <p class="discount"></p>
                 <h2 class="item-text">
                   <span class="flag flag__sweden"></span>
-                  <span class="type type__mediumtank"></span>
+                  <span class="tank-type tank-type__mediumtank"></span>
                   <span class="level">X</span>
                   <span class="item-name">M56</span>
                 </h2>
-                <p class="price">$ 147.99</p>
+                <p class="price">$ 0.99</p>
+                <p class="price price-discount"></p>
               </div>
             </a>
             <button class="like-btn like-btn__active">
+              <svg class="like-btn__icon">
+                <use xlink:href="assets/images/sprite.svg#like"></use>
+              </svg>
+            </button>
+            <button class="purchase-btn">purchase</button>
+          </article>
+                    <article class="card card__single">
+            <a href="#" class="card-info">
+              <img class="card-img" src="assets/images/M56.png" alt="gold" />
+              <div class="card-specifications">
+                <p class="discount"></p>
+                <h2 class="item-text">
+                  <span class="item-name">360 дней танкового премиум аккаунта</span>
+                </h2>
+                <p class="price">$ 100.99</p>
+                <p class="price price-discount"></p>
+              </div>
+            </a>
+            <button class="like-btn">
               <svg class="like-btn__icon">
                 <use xlink:href="assets/images/sprite.svg#like"></use>
               </svg>
@@ -141,4 +165,4 @@ class mainPageComponent {
   }
 }
 
-export default new mainPageComponent();
+export default new MainPageComponent();
--- src/components/product-item/product-item.ts
+++ src/components/product-item/product-item.ts
@@ -1,38 +1,51 @@
-class productItemComponent {
-  card_size='card__single'; // size on table card__single(1) or card__double(2)
-  card_id=''; // item id __ "23525"
-  card_href=''; // link to item page
-  card_src=''; // item img link
-  card_alt=''; // item img description (card_name) __ M56
-  card_discount=''; // item discount (optional) __ -10%
-  card_flag=''; // item flag (optional) __ (flag__china, flag__czech, flag__france, flag__germany, flag__poland, flag__sweden, flag__uk, flag__usa, flag__ussr)
-  card_type=''; // item type (optional) __ (type__at-spg, type__heavytank, type__lighttank, type__mediumtank, type__multirole, type__spg)
-  card_level=''; // item level (optional) __ (I-X)
-  card_name=''; // item name __ M56
-  card_price=''; // item price __ $ 99.99
+class ProductItemComponent {
+  card_size = 'card__single'; // size on table card__single(1) or card__double(2)
+  card_id = ''; // item id __ "23525"
+  card_href = ''; // link to item page
+  card_src = ''; // item img link
+  card_alt = ''; // item img description (card_name) __ M56
+  card_discount = ''; // item discount (optional) __ -10%
+  card_flag = ''; // item flag (optional)
+  // flag__china, flag__czech, flag__france, flag__germany, flag__poland, flag__sweden, flag__uk, flag__usa, flag__ussr
+  card_tank_type = ''; // item type (optional)
+  // __ (type__at-spg, type__heavytank, type__lighttank, type__mediumtank, type__multirole, type__spg)
+  card_level = ''; // item level (optional) __ (I-X)
+  card_name = ''; // item name __ M56
+  card_price = ''; // item price __ $ 99.99
+  card_price_discount = ''; // item price discount __ $ 99.99
+  card_type = ''; // __ tank, gold, prem
   constructor() {
     this.init = this.init.bind(this);
     this.render = this.render.bind(this);
   }
 
-  init() {
-
-  }
+  init() {}
 
   render() {
+    let TankNameInfo;
+    if (this.card_type === 'tank') {
+      TankNameInfo = `
+                  <span class="flag ${this.card_flag}"></span>
+                  <span class="tank-type ${this.card_tank_type}"></span>
+                  <span class="level">${this.card_level}</span>
+                  <span class="item-name">${this.card_name}</span>
+      `;
+    } else {
+      TankNameInfo = `
+                  <span class="item-name">${this.card_name}</span>
+      `;
+    }
     return `
-          <article class="card ${this.card_size}" id="${(this.card_id)}">
-            <a href="${(this.card_href)}" class="card-info">
-              <img class="card-img" src="${this.card_src}" alt="${(this.card_alt)}" />
+          <article class="card ${this.card_size}" id="${this.card_id}">
+            <a href="${this.card_href}" class="card-info">
+              <img class="card-img" src="${this.card_src}" alt="${this.card_alt}" />
               <div class="card-specifications">
                 <p class="discount">${this.card_discount}</p>
                 <h2 class="item-text">
-                  <span class="flag ${this.card_flag}"></span>
-                  <span class="type ${this.card_type}"></span>
-                  <span class="level">${this.card_level}</span>
-                  <span class="item-name">${this.card_name}</span>
+                  ${TankNameInfo}
                 </h2>
                 <p class="price">${this.card_price}</p>
+                <p class="price price-discount">${this.card_price_discount}</p>
               </div>
             </a>
             <button class="like-btn">
@@ -46,4 +59,4 @@ class productItemComponent {
   }
 }
 
-export default new productItemComponent();
+export default new ProductItemComponent();
