f54800a79b9df7e70cc6095caf5151546680386c
Ivan Kuliasov <dfyreybr@gmail.com>
2021-12-09 01:01:22 +0300

    feat: add favorite template

--- src/components/app/components/router/routes/routes.ts
+++ src/components/app/components/router/routes/routes.ts
@@ -1,8 +1,8 @@
 import { mainPage } from '../pages/main-page';
 import { signinPage } from '../pages/signin-page';
 import { signupPage } from '../pages/signup-page';
+import { favoritesPage } from '../pages/favorites-page';
 import { errorPage } from '../pages/error-page';
-import { productPage } from '../pages/product-page';
 
 import { IRoute } from '../models/route.model';
 
@@ -10,8 +10,8 @@ const routes: IRoute[] = [
   { path: '/', page: mainPage },
   { path: '/signin', page: signinPage },
   { path: '/signup', page: signupPage },
+  { path: '/favorites', page: favoritesPage },
   { path: '/error', page: errorPage },
-  { path: '/product/', page: productPage },
 ];
 
 export default routes;
